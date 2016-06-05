ebfc18afc84341edbe4fa95b2296653a8b282135
NikAlex <alez90@yandex.ru>
2016-06-05 22:28:27 +0400

    Update 2.cpp

--- 2.cpp
+++ 2.cpp
@@ -1,83 +1,38 @@
 #include <iostream>
+#include <stdlib.h>
 #include <fstream>
 #include <string>
-
+#include <stdio.h>
+#include <conio.h>
 using namespace std;
 
-int main() {
-	bool fOpen = false;			
-	bool fClose = false;			
-	bool fFunMult = false;
-	bool oneString = false;			
-	string sIn;					
-	string result;					
-	string resultMultiline = "";		
-	size_t pos;			
-	size_t posOp, posCl, posFun;		
-	int k = 0, n = 0, i = 0;		
-	ifstream f_in("test.txt");		
-	ofstream f_out("test.txt", fstream::app);	
-	if (!f_in)					
-		cout << "Error!";
-
-	f_out << '\n';				
-	while (f_in) {				
-		getline(f_in, sIn);		
-		if (fFunMult == false) {		
-			posOp = sIn.find("/**");	
-			if (posOp != std::string::npos) {
-				fOpen = true;	
-				posCl = sIn.find("*/");	
-				if (posCl != std::string::npos) {
-					fClose = true;	
-					oneString = true;	
-				}
-				else
-					oneString = false;	
-			}
-
-			if (oneString == true) {		
-				posFun = sIn.find("\\func");	
-				if (posFun != std::string::npos) {
-					k += 1;			
-					result = sIn.substr(posFun + 6, posCl - (posFun + 6));	
-												
-					f_out << k << ". " << result << endl;			
-					i = 0;
-				}
-				oneString = false;	
-			}
-
-			else if (fFunMult == false) {		
-				posFun = sIn.find("\\func");	
-				if (posFun != std::string::npos) {
-					fFunMult = true;
+int main(){
+	ifstream fin; int k = 0;
+	char a[20]; string str;
+	cout << "name of file: "; cin >> a;
+	fin.open(a);
+	while (!fin.eof()){
+		getline(fin, str); 
+		if (str == "/**"){
+			while (str != "*/"){
+				getline(fin, str);
+				if (str != "*/") { 
+					if (str.find("\\func") != -1){ 
+						k++;
+						cout << k <<". "<< str.substr(str.find("\\func") + 6) << endl; }
 				}
 			}
 		}
-		if (fFunMult == true) {			
-			posCl = sIn.find("*/");			
-			if (posCl != std::string::npos) {	
-				k += 1;				
-				f_out << k << ". " << resultMultiline << endl;	
-				i = 0;
-				fFunMult = false;		
-				fClose = true;			
-				fOpen = false;			
-				resultMultiline = "";  		
-			}
-			else {
-				if (i == 0) {			
-								
-					resultMultiline = resultMultiline + sIn.substr(posFun + 6, posCl - (posFun + 6)) + '\n';
-					i++;		
-				}
-				else
-					resultMultiline = resultMultiline + sIn;
+		if (str.find("/**")!=-1){
+			if (str.find("\\func")!=-1){
+				k++;
+				cout<< k <<". "<< str.substr(str.find("\\func") + 6,str.length()-str.find("\\func")-9)<<endl;
 			}
 		}
 	}
-	f_in.close();
+	fin.close();
+	getch();
 	return 0;
 }
+
 
